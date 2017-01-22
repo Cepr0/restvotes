ae8f855194974c74d8da8dfa64eb5eb0032b47ac
Cepr0 <cepr0@ukr.net>
2017-01-22 16:46:49 +0200

    added some search methods to RestaurantRepo and UserRepo

--- src/main/java/restvotes/repository/RestRepo.java
+++ src/main/java/restvotes/repository/RestRepo.java
@@ -1,8 +1,12 @@
 package restvotes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import restvotes.domain.entity.Restaurant;
+
+import java.util.List;
 
 /**
  * @author Cepro, 2017-01-01
@@ -10,4 +14,10 @@ import restvotes.domain.entity.Restaurant;
 @RepositoryRestResource
 public interface RestRepo extends JpaRepository<Restaurant, Long> {
     // TODO Add RestaurantController and getMenusByRestaurantId() for api/restaurants/{id}/menus
+    
+    @RestResource(path = "byName", rel = "byName")
+    List<Restaurant> findByNameLikeIgnoreCase(@Param("name") String name);
+    
+    @RestResource(path = "byAddress", rel = "byAddress")
+    List<Restaurant> findByAddressLikeIgnoreCase(@Param("address") String name);
 }
--- src/main/java/restvotes/repository/UserRepo.java
+++ src/main/java/restvotes/repository/UserRepo.java
@@ -1,13 +1,13 @@
 package restvotes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import restvotes.domain.entity.User;
 
-import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Cepro, 2017-01-02
@@ -15,21 +15,11 @@ import java.time.LocalDate;
 @RepositoryRestResource
 public interface UserRepo extends JpaRepository<User, Long> {
     
-    @RestResource(exported = false)
-    @Query("select v from Vote v join v.user u where v.poll = current_date and u.email = ?1")
-    User hasVotedTodayByEmail(String email);
+    // TODO Add search for Users by: role, enabled
     
-    default int hasVotedToday(String email) {
-        return hasVotedByEmail(LocalDate.now(), email);
-    }
+    @RestResource(path = "byEmail", rel = "byEmail")
+    Optional<User> findByEmail(@Param("email") String email);
     
-    @RestResource(exported = false)
-    @Query(value = "select count(v.id) from votes v join users u on u.id = v.user_id and u.email = ?2 where v.poll_date = ?1", nativeQuery = true)
-    int hasVotedByEmail(LocalDate date, String email);
-    
-    @RestResource(exported = false)
-    @Query("select count(v) as count from Vote v join v.user u where u.id = :id and v.poll = current_date")
-    int hasVotedTodayById(@Param("id") Long id);
-    
-    // TODO Add search for Users by: name, email, role, enabled
+    @RestResource(path = "byName", rel = "byName")
+    List<User> findByNameLikeIgnoreCase(@Param("name") String name);
 }
